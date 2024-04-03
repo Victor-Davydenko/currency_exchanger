4e8d142b2106e415c356dc2881348d119bbcad21
Victor Davydenko <acc.davydenko@gmail.com>
2024-04-03 18:52:17 +0300

    set custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#707C87',
+        secondary: '#F6F7FF',
+        blue: '#2C36F2',
+        grey: '#C1C2CA',
+        black: '#1F1E25'
+
+
+      }
+    },
   },
   plugins: [],
 }
