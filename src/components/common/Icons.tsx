6c08120eb0aff87cb9472bb162b54a88117b8bc6
Victor Davydenko <acc.davydenko@gmail.com>
2024-04-04 18:46:42 +0300

    add icons

--- src/components/common/Icons.tsx
+++ src/components/common/Icons.tsx
@@ -1,5 +1,17 @@
 import loginIcon from '@/app/icons/login_icon.svg'
+import cellPhone from '@/app/icons/cellphone.svg'
+import phone from '@/app/icons/phone.svg'
+import facebook from '@/app/icons/facebook.svg'
+import instagram from '@/app/icons/instagram.svg'
+import twitter from '@/app/icons/twitter.svg'
+import youtube from '@/app/icons/youtube.svg'
 
 export const Icons = {
-    login: loginIcon
+    login: loginIcon,
+    cellPhone,
+    phone,
+    facebook,
+    instagram,
+    twitter,
+    youtube
 }
