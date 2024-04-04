5ef7c001d2d7569a7c5e3a7e3df856ad2cc3996f
Victor Davydenko <acc.davydenko@gmail.com>
2024-04-04 15:40:33 +0300

    change font

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react';
 import Image from "next/image";
 import Link from "next/link";
+import { work_sans } from "@/app/fonts";
 
 const Logo: FC = () => {
     return (
         <Link href='/' className='flex gap-x-3 items-center'>
             <Image src='/logo.jpg' alt='logo' width={23} height={23} />
-            <strong className='text-black font-bold text-xl'>Чіп Чендж</strong>
+            <strong className={`${work_sans.className} text-black font-bold text-xl`}>Чіп Чендж</strong>
         </Link>
     );
 };
