5a8ab71d83e6cbca0987dc9a44dc5809f515207e
Victor Davydenko <acc.davydenko@gmail.com>
2024-04-05 21:24:44 +0300

    home page finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+import Hero from "@/components/Hero";
+import Image from "next/image";
+import Title from "@/components/common/Title";
+import Button from "@/components/ui/Button";
+import React from "react";
+
 export default function Home() {
   return (
-    <></>
+  <>
+    <Hero/>
+    <section>
+      <div className='container mx-auto max-w-[885px] flex py-[120px]'>
+        <div className='basis-1/2 pr-[50px]'>
+          <Title className={'text-black font-bold text-[40px] leading-[56px] mb-5'} level={2}>Конвертер валют</Title>
+          <p className='font-normal text-xl text-primary mb-7'>Переважна діяльність банківської групи за останні чотири звітні квартали становить 50 і більше відсотків.</p>
+          <Button variant='small-blue' href='/converter'>Конвертер валют</Button>
+        </div>
+        <div className='basis-1/2'>
+          <Image src='/promo_img.jpg' alt='card' width={440} height={315} />
+        </div>
+      </div>
+    </section>
+  </>
   );
 }
