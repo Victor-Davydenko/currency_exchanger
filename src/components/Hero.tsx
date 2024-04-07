5ef76a62897753495c3cd898557901ad4ff54610
Victor Davydenko <acc.davydenko@gmail.com>
2024-04-07 19:14:48 +0300

    add priority to prefetch hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import Button from "@/components/ui/Button";
 const Hero: FC = (): ReactElement => {
     return (
         <section className='relative'>
-            <Image src='/hero_bg.jpg' alt='hero_bg_img' width={1500} height={400} className='absolute z-[-1] w-full max-h-[400px]' />
+            <Image src='/hero_bg.jpg' alt='hero_bg_img' priority={true} width={1500} height={400} className='absolute z-[-1] w-full max-h-[400px]' />
             <div className='container mx-auto max-w-[885px] flex pt-[85px] pb-[100px]'>
                 <div className='basis-1/2'>
                     <Title className={'text-secondary font-bold text-[54px] leading-[64px] mb-5'} level={1}>Чіп Чендж</Title>
@@ -14,7 +14,7 @@ const Hero: FC = (): ReactElement => {
                     <Button variant='small-grey' href='/converter'>Конвертер валют</Button>
                 </div>
                 <div className='basis-1/2'>
-                    <Image src='/credit_card.jpg' alt='card' width={340} height={215} />
+                    <Image src='/credit_card.jpg' alt='card' width={340} height={215} className='rounded-md'/>
                 </div>
             </div>
         </section>
