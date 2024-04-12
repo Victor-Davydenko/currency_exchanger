c0d541343738eef344463959cee9218c70ce2bf4
Victor Davydenko <acc.davydenko@gmail.com>
2024-04-12 16:30:46 +0300

    add history

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from 'react';
 import HistoryItem from '@/components/HistoryItem';
-import { history } from '@/components/HistoryBlock';
+import {HistoryData} from "@/interfaces/interfaces";
+import useConverterStore from "@/store/store";
+
 
 const History: FC = () => {
+  const history: HistoryData[] = useConverterStore(state => state.history)
   return (
     <div className='flex flex-wrap justify-between gap-x-12 gap-y-4'>
       {history.map(({ date, amountToBuy, amountToSell }, idx) => <HistoryItem key={idx}
-                                                                                                           date={date}
-                                                                                                           amountToBuy={amountToBuy}
-                                                                                                           amountToSell={amountToSell}
+                                                                                                             date={date}
+                                                                                                             amountToBuy={amountToBuy}
+                                                                                                             amountToSell={amountToSell}
       />)}
     </div>
   );
