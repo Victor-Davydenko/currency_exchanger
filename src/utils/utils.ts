16c933e3e23a88bd3daafe595b1f96e9016c15ab
Victor Davydenko <acc.davydenko@gmail.com>
2024-04-13 18:52:37 +0300

    change methods to work with date

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,8 @@
+import {format} from 'date-fns'
+
 export const getCurrentDate = () => {
   const date = new Date()
-  return date.toLocaleDateString().split('.').reverse().join('-')
+  return format(new Date(), 'yyyy-MM-dd')
 }
 
 export const reverseDate = (date: string) => date.split('-').reverse().join('-')
@@ -8,10 +10,10 @@ export const reverseDate = (date: string) => date.split('-').reverse().join('-')
 export const removeDashesFromDateString = (date: string) => date.split('-').join('')
 
 export const getMaxAndMinDate = (range: number) => {
-  const maxDate = new Date().toISOString().split('T')[0]
+  const maxDate = format( new Date(), 'yyyy-MM-dd')
   const minDate = new Date();
   minDate.setDate(minDate.getDate() - range)
-  const minDateString = minDate.toISOString().split('T')[0];
+  const minDateString = format(minDate, 'yyyy-MM-dd')
   return {
     maxDate,
     minDateString
