4cb29aa06e2d5098017ab191f4c2dfca42f2655d
Victor Davydenko <acc.davydenko@gmail.com>
2024-10-02 09:45:54 +0300

    add loading and error

--- src/components/ConverterForm.tsx
+++ src/components/ConverterForm.tsx
@@ -13,12 +13,15 @@ import {ISelectOption, RatesResponseItem, IForm} from '@/interfaces/interfaces';
 import {addToHistorySelector} from '@/store/selectors';
 import {converterFormValidationSchema} from '@/validation/converterFormValidationSchema';
 import {Icons} from "@/components/common/Icons";
+import Loading from "@/app/loading";
 
 const { maxDate, minDateString } = getMaxAndMinDate(10)
 const ConverterForm = () => {
   const addToHistory = useConverterStore(addToHistorySelector)
   const [rate, setRate] = useState(1)
   const [rates, setRates] = useState<RatesResponseItem[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   const {control, handleSubmit, watch, setValue} = useForm<IForm>({
     defaultValues: {
@@ -41,6 +44,7 @@ const ConverterForm = () => {
 
   useEffect(() => {
     const formattedDate = removeDashesFromDateString(date)
+    setIsLoading(true)
     getRates(formattedDate)
       .then((rates) => {
         setRates(rates)
@@ -48,6 +52,8 @@ const ConverterForm = () => {
         const currencyToBuyRate = rates.find((currency) => currency.cc === currencyToBuy)?.rate || 1
         setRate(currencyToSellRate / currencyToBuyRate)
       })
+      .catch((e) => setError(e))
+      .finally(() => setIsLoading(false))
   },[date])
 
   useEffect(() => {
@@ -77,77 +83,87 @@ const ConverterForm = () => {
     })
   }
 
+  if(error) {
+    return (
+      <div>errror</div>
+    )
+  }
+
   return (
-    <form className='flex relative' onSubmit={handleSubmit(onFormSubmit)}>
-      <Icons.arrow_icon className='absolute left-1/2 transform -translate-x-1/2 w-[40px] h-[40px] top-2 z-10'></Icons.arrow_icon>
-      <div className='basis-1/2 flex flex-wrap gap-x-4 gap-y-6'>
-        <Controller
-          name='amountToSell'
-          control={control}
-          render={({ field: { value} }) => <Input
-            id='amountToSell'
-            type='number'
-            step='any'
-            min='0'
-            className='appearance-none outline outline-0 focus:outline-0 border-grey rounded border-[1px] h-[60px] w-[215px] text-center font-medium text-primary text-xl [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none'
-            label='В мене є:'
-            value={value}
-            handler={handleAmountToSellInput}
-          />}
-        />
-        <Controller
-          name='currencyToSell'
-          control={control}
-          render={({ field: { value, onChange} }) => <Select
-            options={availableCurrencies}
-            value={value}
+    <>
+      {isLoading && <Loading />}
+      <form className='flex relative' onSubmit={handleSubmit(onFormSubmit)}>
+        <Icons.arrow_icon
+          className='absolute left-1/2 transform -translate-x-1/2 w-[40px] h-[40px] top-2 z-10'></Icons.arrow_icon>
+        <div className='basis-1/2 flex flex-wrap gap-x-4 gap-y-6'>
+          <Controller
+            name='amountToSell'
+            control={control}
+            render={({field: {value}}) => <Input
+              id='amountToSell'
+              type='number'
+              step='any'
+              min='0'
+              className='appearance-none outline outline-0 focus:outline-0 border-grey rounded border-[1px] h-[60px] w-[215px] text-center font-medium text-primary text-xl [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none'
+              label='В мене є:'
+              value={value}
+              handler={handleAmountToSellInput}
+            />}
+          />
+          <Controller
             name='currencyToSell'
-            handler={onChange}
-          />}
-        />
-        <Controller
-          name='date'
-          control={control}
-          render={({ field: {value, onChange} }) => <Input
-            type='date'
-            id='date'
-            min={minDateString}
-            max={maxDate}
-            label={''}
-            value={value}
-            className='appearance-none outline outline-0 focus:outline-0 border-grey rounded border-[1px] h-[60px] w-[215px] text-center font-medium text-primary text-xl [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none  [&::-webkit-calendar-picker-indicator]:opacity-0 [&::-webkit-calendar-picker-indicator]:w-[25px]'
-            handler={onChange}
-            icon={<Icons.calendar className='absolute top-4 right-3 w-[25px] h-[25px]' />}
-          />}
-        />
-      </div>
-      <div className='basis-1/2 flex flex-wrap gap-x-4 gap-y-6 last:justify-end'>
-        <Controller
-          name='amountToBuy'
-          control={control}
-          render={({ field: {value} }) => <Input
-            step='any'
-            min='0'
-            value={value}
-            id='amountToBuy'
-            type='number'
-            className='appearance-none outline outline-0 focus:outline-0 border-grey rounded border-[1px] h-[60px] w-[215px] text-center font-medium text-primary text-xl [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none'
-            label='Хочу придбати:'
-            handler={handleAmountToBuyInput}
-          />}
-        />
-        <Controller
-          name='currencyToBuy'
-          control={control}
-          render={({ field: {value, onChange} }) => <Select
-            options={availableCurrencies}
+            control={control}
+            render={({field: {value, onChange}}) => <Select
+              options={availableCurrencies}
+              value={value}
+              name='currencyToSell'
+              handler={onChange}
+            />}
+          />
+          <Controller
+            name='date'
+            control={control}
+            render={({field: {value, onChange}}) => <Input
+              type='date'
+              id='date'
+              min={minDateString}
+              max={maxDate}
+              label={''}
+              value={value}
+              className='appearance-none outline outline-0 focus:outline-0 border-grey rounded border-[1px] h-[60px] w-[215px] text-center font-medium text-primary text-xl [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none  [&::-webkit-calendar-picker-indicator]:opacity-0 [&::-webkit-calendar-picker-indicator]:w-[25px]'
+              handler={onChange}
+              icon={<Icons.calendar className='absolute top-4 right-3 w-[25px] h-[25px]'/>}
+            />}
+          />
+        </div>
+        <div className='basis-1/2 flex flex-wrap gap-x-4 gap-y-6 last:justify-end'>
+          <Controller
+            name='amountToBuy'
+            control={control}
+            render={({field: {value}}) => <Input
+              step='any'
+              min='0'
+              value={value}
+              id='amountToBuy'
+              type='number'
+              className='appearance-none outline outline-0 focus:outline-0 border-grey rounded border-[1px] h-[60px] w-[215px] text-center font-medium text-primary text-xl [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none'
+              label='Хочу придбати:'
+              handler={handleAmountToBuyInput}
+            />}
+          />
+          <Controller
             name='currencyToBuy'
-            handler={onChange} value={value}
-          />}
-        />
-        <Button variant={`${isValid ? 'small-blue': 'disabled'}`}>Зберегти результат</Button>
-      </div>
-    </form>
+            control={control}
+            render={({field: {value, onChange}}) => <Select
+              options={availableCurrencies}
+              name='currencyToBuy'
+              handler={onChange} value={value}
+            />}
+          />
+          <Button variant={`${isValid ? 'small-blue' : 'disabled'}`}>Зберегти результат</Button>
+        </div>
+      </form>
+    </>
   );
 };
 
